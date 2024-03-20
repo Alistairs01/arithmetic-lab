ab1f5df41ce76da2ff08f93dce28fa0a68633e35
Alistairs <alistairs.gatheru@student.moringaschool.com>
2024-03-20 09:07:19 +0300

    arithmetic-labs

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-
-// Write your code here
+// create variables num1 and num2 who multiplication equal to 62
+let num1 = 31;
+let num2 =  2;
+// create a variable for multiply
+const multiply = num1 * num2;
+// create a random integer greater than zero
+let random = Math.floor(Math.random() * 10) + 1;
+// mod variable that will calculate the remainder of dividing variables num3 and num4
+let num3 = 10;
+let num4 = 6;
+// create the mod function
+const mod = num3 % num4;
+// create a variable that will return the highest number 20 max
+let max = Math.max(10,15,18,20);
+// calling the variables
+console.log(multiply)
+console.log(random)
+console.log(mod)
+console.log(max)
